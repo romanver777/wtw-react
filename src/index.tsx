8fc355d290ddfa769799d04bdebbdd28b110c1f9
romanver <romanver777@yandex.ru>
2021-05-19 21:07:03 +0300

    add history

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { Router } from "react-router-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 
 import "./style/style.css";
-// import films from "./mocks/films.json";
 import App from "./components/app/app";
 import { reducer } from "./reducer";
+import history from "./history";
 
 const store = createStore(reducer);
 
 render(
   <Provider store={store}>
-    <BrowserRouter>
+    <Router history={history}>
       <App />
-    </BrowserRouter>
+    </Router>
   </Provider>,
   document.getElementById("root")
 );

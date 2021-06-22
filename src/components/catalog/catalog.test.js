899d7565874a68a38385511e27d84d6ef7b07428
romanver <romanver777@yandex.ru>
2021-06-22 18:37:56 +0300

    add Catalog component tests

---
+++ src/components/catalog/catalog.test.js
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import React from "react";
+import TestRenderer from "react-test-renderer";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { reducer } from "../../reducer";
+import { Router } from "react-router";
+import history from "../../history";
+
+import Catalog from "./catalog";
+import films from "../../mocks/films.json";
+import { ALL_GENRES } from "../../helpers/const";
+
+const initState = {
+  films,
+  awaitFilm: films[2],
+  currentFilm: null,
+  hoveredFilm: null,
+  activeGenre: ALL_GENRES,
+  genresList: null,
+  tk: null,
+  reviews: null,
+  staff: null,
+  hoveredVideoData: null,
+};
+
+const store = createStore(reducer, initState);
+
+describe.only("Catalog component", () => {
+  const props = { pageName: "main" };
+  const Trenderer = TestRenderer.create(
+    <Provider store={store}>
+      <Router history={history}>
+        <Catalog {...props} />
+      </Router>
+    </Provider>
+  );
+
+  it("renders correctly", () => {
+    expect(Trenderer.toJSON()).toMatchSnapshot();
+  });
+});

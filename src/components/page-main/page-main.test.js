6afe4f2434a5f46c84dd2e43a0c2ec1b0598fce4
romanver <romanver777@yandex.ru>
2022-04-21 17:13:31 +0300

    update tests

--- src/components/page-main/page-main.test.js
+++ src/components/page-main/page-main.test.js
@@ -2,34 +2,35 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React from "react";
-import ShallowRenderer from "react-test-renderer/shallow";
+import TestRenderer from "react-test-renderer";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { Router } from "react-router";
+import history from "../../history";
+import { reducer } from "../../reducer";
 
 import PageMain from "./page-main";
+import { ALL_GENRES } from "../../helpers/const";
+import films from "../../mocks/films.json";
+
+const initState = {
+  films,
+  awaitFilm: films[1],
+  currentFilm: films[0],
+  activeGenre: ALL_GENRES,
+  genresList: ["Все жанры", "Комедия", "Фантастика"],
+};
+const store = createStore(reducer, initState);
 
 describe("PageMain component", () => {
-  const renderer = new ShallowRenderer();
-  renderer.render(<PageMain />);
-  const result = renderer.getRenderOutput();
-
-  it("renders first child as MovieCard", () => {
-    expect(result.props.children[0].type.type.displayName).toEqual(
-      "Connect(MovieCard)"
-    );
-  });
-  describe("renders second child", () => {
-    it("as div", () => {
-      expect(result.props.children[1].type).toEqual("div");
-    });
-    it("with className", () => {
-      expect(result.props.children[1].props.className).toEqual("page-content");
-    });
-    it("with 2 children", () => {
-      expect(result.props.children[1].props.children).toHaveLength(2);
-    });
-    it("with first children props", () => {
-      expect(result.props.children[1].props.children[0].props.pageName).toEqual(
-        "main"
-      );
-    });
+  const Trenderer = TestRenderer.create(
+    <Provider store={store}>
+      <Router history={history}>
+        <PageMain />
+      </Router>
+    </Provider>
+  );
+  it("renders correctly", () => {
+    expect(Trenderer.toJSON()).toMatchSnapshot();
   });
 });
